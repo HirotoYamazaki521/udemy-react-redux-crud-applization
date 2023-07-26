8c98816fb5827c15334dec19a849e68c861dfe9c
hiroto <yamazakih0551@gmail.com>
2023-07-26 18:38:44 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from './component/App';
 import reportWebVitals from './reportWebVitals';
 
+// アプリケーション内部で唯一のstoreを作成
+// また、アプリケーション内部の全てのステートはこのstoreに集約される
+const store = createStore(reducer); 
+
+// Providerコンポーネントでラップすることで、
+// 全階層のコンポーネントでstoreにアクセスできるようになる
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
